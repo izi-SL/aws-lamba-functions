28c66d24e08bcaf9896d3ab5454b8470cf40630b
Isuru Gajanayake <isuru.gajanayake@gmail.com>
2017-06-18 22:39:38 +0530

    Initial commit

---
+++ src/main/java/com/isuru/lambda/PrintFunction.java
@@ -0,0 +1,12 @@
+package com.isuru.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class PrintFunction implements RequestHandler<String, String>{
+
+	public String handleRequest(String input, Context context) {
+		return input.toUpperCase();
+	}
+  
+}
